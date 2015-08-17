1782c381fddd69524a74967b06e2b7c4c0a8fd87
Hafidz Amrulloh <assasinba404@gmail.com>
2015-08-17 23:39:38 +0700

    Update Hello World Windows Form.cs

---
+++ C# Windows Form/Hello World Windows Form.cs
@@ -0,0 +1,26 @@
+//File Header
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Name Space
+namespace FirstWindowsForm
+{
+    //Class
+    public partial class fpGundarTi : Form
+    {
+        //Fucntion / Method Public Form
+        public fpGundarTi()
+        {
+            InitializeComponent();
+            //Untuk Menampilkan Pesan Hello World Ke Layar
+            MessageBox.Show("Hello World");
+        }
+    }
+}
